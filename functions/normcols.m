7b86c7408d35368b7609df0c8f320216d44423be
Sposmotic <125043341+Sposmotic@users.noreply.github.com>
2025-05-16 10:48:52 +0800

    Update normcols.m

--- functions/normcols.m
+++ functions/normcols.m
@@ -1,15 +1,15 @@
 function matout = normcols(matin)
-% 对'matin'的每一列进行归一化，使得每列的L2范数为1。
-% 输入:
-%       matin   - 一个矩阵
-% 输出:
-%       matout  - 一个矩阵，每列的L2范数为1
+% Normalizes each column of 'matin' to have unit L2 norm (Euclidean norm).
+% Input:
+%       matin   - A matrix
+% Output:
+%       matout  - A matrix with each column normalized to L2 norm = 1
 
-% 计算每列的L2范数，即每列元素的平方和的平方根
+% Compute L2 norms of columns (square root of sum of squares for each column)
 l2norms = sqrt(sum(matin.^2,1));
 
-% 如果某列的L2范数为0（即该列全为零），则将L2范数设置为一个很小的值（eps，防止除以零）
+% Replace zero norms with eps to prevent division by zero
 l2norms(l2norms==0) = eps;
 
-% 将每列除以其对应的L2范数，实现列归一化
+% Normalize each column by dividing by its L2 norm
 matout = bsxfun(@rdivide,matin,l2norms);
